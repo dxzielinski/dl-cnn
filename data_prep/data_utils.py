20214f1a7b7ebbc8fc6d214effe1c5c7437bede8
dxzielinski <dominik.zielinski2002@gmail.com>
2025-03-17 23:10:36 +0100

    migrate to lightning

--- data_prep/data_utils.py
+++ data_prep/data_utils.py
@@ -8,6 +8,9 @@ default_transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomVerticalFlip(p=0.5),
     transforms.RandomRotation(degrees=20),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+    transforms.RandomErasing(p=0.5, scale=(0.005, 0.01), ratio=(1.2, 1.8)),  # cutout
+    # 32*32 = 1024; 0.005 * 1024 ~= 5; 3x2 has ratio 1.5, so ratio can be 1.2-1.8
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.4789, 0.4723, 0.4305], std=[0.2421, 0.2383, 0.2587])
 ])
--- model/model_utils.py
+++ model/model_utils.py
@@ -1,90 +1,200 @@
 import torch
-import mlflow
+import lightning as L
+import torchmetrics
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+from model_config import TASK, NUM_CLASSES
 
 
-def _calc_conv_output_size(size=32, kernel_size=3, stride=1, padding=0, pooling_size=2):
+def _calc_conv_output_size(size=32, kernel_size=2, stride=1, padding=0, pooling_size=2):
     conv_out = (size - kernel_size + 2 * padding) // stride + 1
     pool_out = conv_out // pooling_size
     return pool_out
 
 
 class BasicCNN(torch.nn.Module):
-    def __init__(self, learning_rate):
+    def __init__(self, dropout):
         super().__init__()
-        self.model = torch.nn.Sequential(
-            torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1),
+        self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=2, stride=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
-            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1),
+            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
             torch.nn.Flatten(),
             torch.nn.Linear(in_features=64 * _calc_conv_output_size(_calc_conv_output_size()) ** 2, out_features=64),
+            torch.nn.BatchNorm1d(64),
             torch.nn.ReLU(),
+            torch.nn.Dropout(dropout),
             torch.nn.Linear(in_features=64, out_features=10),
         )
-        self.learning_rate = learning_rate
-        self.optimizer = torch.optim.AdamW(self.parameters(), lr=learning_rate)
-        self.criterion = torch.nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Model(L.LightningModule):
+    def __init__(self, hyperparameters):
+        """
+        Args:
+            hyperparameters: dict containing model hyperparameters - it must contain the
+                following keys:
+                - learning_rate: float, learning rate for the optimizer
+                - dropout: float, dropout rate before the last linear layer
+                - weight_decay: float, weight decay for the optimizer
+        """
+        super().__init__()
+        self.model = BasicCNN(hyperparameters["dropout"])
+        self.learning_rate = hyperparameters["learning_rate"]
+        self.train_metrics = torchmetrics.MetricCollection(
+            {
+                "f1_macro": torchmetrics.F1Score(
+                    task=TASK, num_classes=NUM_CLASSES, average="macro"
+                ),
+                "precision": torchmetrics.Precision(
+                    task=TASK, num_classes=NUM_CLASSES, average="macro"
+                ),
+                "recall": torchmetrics.Recall(
+                    task=TASK, num_classes=NUM_CLASSES, average="macro"
+                ),
+                "auroc": torchmetrics.AUROC(
+                    task=TASK, num_classes=NUM_CLASSES, average="macro"
+                ),
+            },
+            prefix="train_",
+        )
+        self.val_metrics = self.train_metrics.clone(prefix="val_")
+        self.test_metrics = self.train_metrics.clone(prefix="test_")
+        self.train_batch_outputs = []
+        self.val_batch_outputs = []
+        self.test_batch_outputs = []
+        self.hyperparameters = hyperparameters
+
+    def on_train_start(self):
+        if self.logger is not None:
+            self.logger.log_hyperparams(self.custom_hparams)
+
+    def on_test_start(self):
+        if self.logger is not None:
+            self.logger.log_hyperparams(self.custom_hparams)
 
     def forward(self, x):
         return self.model(x)
 
-    def fit(self, train_dataset, val_dataset, device="cuda", epochs=10):
-        self.to(device)
-        self.train()
-        mlflow.start_run()
-        mlflow.log_param("learning_rate", self.learning_rate)
-        mlflow.log_param("epochs", epochs)
-        for epoch in range(epochs):
-            self.train()
-            running_loss = 0.0
-            for i, data in enumerate(train_dataset.get_dataloader(), 0):
-                inputs, labels = data
-                self.optimizer.zero_grad()
-                # forward + backward + optimize
-                outputs = self(inputs.to(device))
-                labels = labels.to(device)
-                loss = self.criterion(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-                running_loss += loss.item()
-                if i % 1000 == 999:
-                    avg_train_loss = running_loss / 1000
-                    mlflow.log_metric("train_loss", avg_train_loss)
-                    running_loss = 0.0
-            self.eval()
-            val_loss = 0.0
-            with torch.no_grad():
-                for inputs, labels in val_dataset.get_dataloader():
-                    inputs, labels = inputs.to(device), labels.to(device)
-                    outputs = self(inputs)
-                    loss = self.criterion(outputs, labels)
-                    val_loss += loss.item()
-            avg_val_loss = val_loss / len(val_dataset.get_dataloader())
-            mlflow.log_metric("val_loss", avg_val_loss)
+    def training_step(self, batch):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        probabilities = torch.softmax(logits, dim=1)
+        self.train_batch_outputs.append({"probabilities": probabilities, "y": y})
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        return loss
 
-        mlflow.pytorch.log_model(self, "model")
-        mlflow.end_run()
+    def on_train_epoch_end(self):
+        probabilities = torch.cat(
+            [x["probabilities"] for x in self.train_batch_outputs]
+        )
+        y = torch.cat([x["y"] for x in self.train_batch_outputs])
+        metrics = self.train_metrics(probabilities, y)
+        self.log_dict(metrics)
+        self.train_metrics.reset()
+        self.train_batch_outputs.clear()
 
-    def evaluate(self, test_dataset, device="cuda"):
-        self.to(device)
-        self.eval()
-        classes = test_dataset.get_classes()
-        correct_pred = {classname: 0 for classname in classes}
-        total_pred = {classname: 0 for classname in classes}
-        with torch.no_grad():
-            for data in test_dataset.get_dataloader():
-                images, labels = data
-                labels = labels.to(device)
-                images = images.to(device)
-                outputs = self(images)
-                _, predictions = torch.max(outputs, 1)
-                for label, prediction in zip(labels, predictions):
-                    if label == prediction:
-                        correct_pred[classes[label]] += 1
-                    total_pred[classes[label]] += 1
+    def validation_step(self, batch):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        probabilities = torch.softmax(logits, dim=1)
+        self.val_batch_outputs.append({"probabilities": probabilities, "y": y})
+        self.log("val_loss", loss, on_step=True, on_epoch=True)
+        return loss
 
-        return correct_pred, total_pred
+    def on_validation_epoch_end(self):
+        probabilities = torch.cat([x["probabilities"] for x in self.val_batch_outputs])
+        y = torch.cat([x["y"] for x in self.val_batch_outputs])
+        metrics = self.val_metrics(probabilities, y)
+        self.log_dict(metrics)
+        self.val_metrics.reset()
+        self.val_batch_outputs.clear()
+
+    def test_step(self, batch):
+        x, y = batch
+        logits = self(x)
+        probabilities = torch.softmax(logits, dim=1)
+        self.test_batch_outputs.append({"probabilities": probabilities, "y": y})
+
+    def on_test_epoch_end(self):
+        probabilities = torch.cat([x["probabilities"] for x in self.test_batch_outputs])
+        y = torch.cat([x["y"] for x in self.test_batch_outputs])
+        metrics = self.test_metrics(probabilities, y)
+        self.log_dict(metrics)
+        self.test_metrics.reset()
+        self.test_batch_outputs.clear()
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(
+            self.model.parameters(),
+            lr=self.learning_rate,
+            weight_decay=self.hyperparameters["weight_decay"],
+        )
+        
+
+default_transforms = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=20),
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+    transforms.RandomErasing(p=0.5, scale=(0.005, 0.01), ratio=(1.2, 1.8)),  # cutout
+    # 32*32 = 1024; 0.005 * 1024 ~= 5; 3x2 has ratio 1.5, so ratio can be 1.2-1.8
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.4789, 0.4723, 0.4305], std=[0.2421, 0.2383, 0.2587])
+])
+
+
+class ClassificationData(L.LightningDataModule):
+
+    def __init__(self, data_dir="../data", batch_size=16):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        if stage == "fit":
+            self.train_dataset = ImageFolder(
+                root=f"{self.data_dir}/train", transform=default_transforms
+            )
+            self.val_dataset = ImageFolder(
+                root=f"{self.data_dir}/val", transform=default_transforms
+            )
+        if stage == "test":
+            self.test_dataset = ImageFolder(
+                root=f"{self.data_dir}/test", transform=default_transforms
+            )
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=8,
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=8,
+        )
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=8,
+        )
